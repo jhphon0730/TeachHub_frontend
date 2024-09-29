990d505e1fb1269be2c37ab9c0766096be52fcbe
jhphon0730 <eeung025@gmail.com>
2024-09-30 00:34:55 +0900

    fix main page bedges Design grid

--- app/page.tsx
+++ app/page.tsx
@@ -73,7 +73,7 @@ export default function Component() {
           </div>
         </section>
         <section className="w-full py-12 md:py-24 lg:py-32 bg-gray-100 dark:bg-gray-800">
-          <div className="container px-4 md:px-6">
+          <div className="container px-4 md:px-6 mx-auto">
             <div className="grid gap-10 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-start justify-center">
               {[
                 "Intuitive Design",
