b0242459a6ddd17ce1d30404baba168b4a4586db
jhphon0730 <eeung025@gmail.com>
2024-10-05 15:54:11 +0900

    feat - Add Redux Provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 
+import ReduxProvider from "@/components/provider/ReduxProvider";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+				<ReduxProvider>
+					{children}
+				</ReduxProvider>
       </body>
     </html>
   );
