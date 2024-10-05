38bd9eb48958fe313c7fb006e44e8f521f589cc3
jhphon0730 <eeung025@gmail.com>
2024-10-05 11:22:48 +0900

    feat - Edit Profile Page tsx file Function Name'

--- app/(auth)/profile/page.tsx
+++ app/(auth)/profile/page.tsx
@@ -1,15 +1,12 @@
 import { Icons } from "@/components/ui/icons"
 
-const AuthEditPage = () => {
+const AuthProfilePage = () => {
 	return (
     <div className="container flex h-screen w-screen flex-col items-center justify-center mx-auto">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
           <Icons.logo className="mx-auto h-6 w-6" />
-          <h1 className="text-2xl font-semibold tracking-tight">Welcome back</h1>
-          <p className="text-sm text-muted-foreground">
-            Enter your email to sign in to your account
-          </p>
+          <h1 className="text-2xl font-semibold tracking-tight">My Profile</h1>
         </div>
 				{/* <AuthForm type="login" onSubmit={handleSubmit} /> */}
       </div>
@@ -17,4 +14,4 @@ const AuthEditPage = () => {
 	)
 }
 
-export default AuthEditPage
+export default AuthProfilePage
