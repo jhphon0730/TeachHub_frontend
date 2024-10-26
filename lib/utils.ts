543b288651a40785a98fe2fc9467752c2f318cd0
jhphon0730 <eeung025@gmail.com>
2024-10-26 22:55:56 +0900

    feat: 쿠키 관련 utils 함수 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,18 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
+import Cookies from 'js-cookie';
+
+export const setCookie = (name: string, value: string, options?: Cookies.CookieAttributes) => {
+  Cookies.set(name, value, options);
+};
+
+export const getCookie = (name: string) => {
+  return Cookies.get(name);
+};
+
+export const removeCookie = (name: string) => {
+  Cookies.remove(name);
+};
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
